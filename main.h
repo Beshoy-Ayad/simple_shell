b84ec13999a6e9b5736630b73e341c16ae1cda21
Sorady <ahmedelsorady2015@gmail.com>
2023-05-22 10:32:26 -0700

    Main

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define MAX_LEN 1024
+#define PROMPT "P:> "
+#define MAX_ARG 64
+#define DELIM " \t\r\n\a "
+
+
+#endif
